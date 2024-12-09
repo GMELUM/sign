1ca53b17f3a2ad7d9fe2aad69aa58e0e1de63ad2
GMELUM <gmelum@vk.com>
2024-12-09 04:32:02 +0300

    review

--- types/tg.go
+++
@@ -1,9 +0,0 @@
-package types
-
-type TGUser struct {
-	ID        int    `msgpack:"id" json:"id"`
-	FirstName string `msgpack:"fn" json:"first_name"`
-	LastName  string `msgpack:"ln" json:"last_name"`
-	UserName  string `msgpack:"un" json:"username"`
-	PhotoURL  string `msgpack:"pu" json:"photo_url"`
-}
